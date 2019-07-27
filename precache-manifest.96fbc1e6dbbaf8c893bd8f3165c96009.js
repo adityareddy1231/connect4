c707847b14b9d88b6906242a3adf3e449f0a6abc
Aditya Reddy <adityareddy1231@gmail.com>
2019-07-27 01:42:31 -0500

    Updates

---
+++ precache-manifest.96fbc1e6dbbaf8c893bd8f3165c96009.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc08e0dcd43c4ba5d21214d94d590520",
+    "url": "/connect4/index.html"
+  },
+  {
+    "revision": "7dbea26c15f426fa5ada",
+    "url": "/connect4/static/css/main.de9f37b8.chunk.css"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/connect4/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "7dbea26c15f426fa5ada",
+    "url": "/connect4/static/js/main.d2a3a5b3.chunk.js"
+  },
+  {
+    "revision": "de9821dd79bb17df15fc",
+    "url": "/connect4/static/js/runtime~main.e9434ad7.js"
+  }
+]);
